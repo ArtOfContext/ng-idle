7ed53d287e52acdbd0bc72d12d40cc5a19f8d190
Tom Dunn <tdunn@artofcontext.com>
2015-05-04 14:02:03 -0400

    For Chrome Apps localStorage is not available.  In that case shim it with an in memory dictionary.

--- angular-idle.js
+++ angular-idle.js
@@ -406,7 +406,21 @@ angular.module('ngIdle.title', [])
 
 angular.module('ngIdle.localStorage', [])
   .service('IdleLocalStorage', ['$window', function($window) {
-    var storage = $window.localStorage;
+    var storage;
+
+    if (chrome !== undefined && chrome.storage !== undefined) {
+      // in case of Chrome App, shim localStorage with simple in memory hash
+      storage = {
+        _data       : {},
+        setItem     : function(id, val) { return this._data[id] = String(val); },
+        getItem     : function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
+        removeItem  : function(id) { return delete this._data[id]; },
+        clear       : function() { return this._data = {}; }
+      };
+    }
+    else {
+      storage = $window.localStorage;
+    }
     
     return {
       set: function(key, value) {
--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -1,6 +1,20 @@
 angular.module('ngIdle.localStorage', [])
   .service('IdleLocalStorage', ['$window', function($window) {
-    var storage = $window.localStorage;
+    var storage;
+
+    if (chrome !== undefined && chrome.storage !== undefined) {
+      // in case of Chrome App, shim localStorage with simple in memory hash
+      storage = {
+        _data       : {},
+        setItem     : function(id, val) { return this._data[id] = String(val); },
+        getItem     : function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
+        removeItem  : function(id) { return delete this._data[id]; },
+        clear       : function() { return this._data = {}; }
+      };
+    }
+    else {
+      storage = $window.localStorage;
+    }
     
     return {
       set: function(key, value) {
